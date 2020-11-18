aec7e8515a8deb9cb37cc742bd418eb95f00780b
syogoizawa <65202759+syogoizawa@users.noreply.github.com>
2020-11-18 14:38:46 +0900

    change finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,68 @@
 // 最終課題を制作しよう
 
+let player = {
+  x: 200,
+  y: 300,
+  vx: 0,
+  vy: 0
+};
+
+let block = {
+  x: 900,
+  y: 300,
+  vx: -2,
+  vy: 0
+};
+
+let blocks;
+
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 600);
+  rectMode(CENTER);
+
+
+  blocks = [];
 }
 
-function draw(){
-  background(160, 192, 255);
+function draw() {
+  if (frameCount % 120 === 1) addBlockPair(blocks);
+
+  player.x += player.vx;
+  player.y += player.vy;
+  block.x += block.vx;
+  block.y += block.vy;
+
+  for (let block of blocks) {
+    block.x += block.vx
+    block.y += block.vy
+  };
+
+  player.vy += 0.15;
+
+  background(0);
+
+  square(player.x, player.y, 40);
+  for (let block of blocks) {
+    rect(block.x, block.y, 80, 400);
+  };
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+function mousePressed(){
+  player.vy = -5;
+}
+
+function addBlockPair() {
+  let y = random(-100, 100);
+  blocks.push({
+    x: 900,
+    y: y,
+    vx: -2,
+    vy: 0
+  }); // 上のブロック
+  blocks.push({
+    x: 900,
+    y: y + 600,
+    vx: -2,
+    vy: 0
+  }); // 下のブロック
 }
